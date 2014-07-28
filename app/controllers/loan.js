1fa1c3fd78ff6b1a3c88c43b986c73804250bb06
Zach McGonigal <zach.mcgonigal@gmail.com>
2014-07-27 23:20:45 -0400

    very primitive calculations

--- app/controllers/loan.js
+++ app/controllers/loan.js
@@ -4,16 +4,37 @@
 
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
 
   /**
    * @property {Boolean} - can show the table with results or not
    */
   canShowTable: false,
 
+  rows: function () {
+    var ret = [],
+        balance = +this.get('balance'),
+        interest = +this.get('interest'),
+        monthly = +this.get('monthly');
+
+    if (monthly && balance) {
+      var monthsToPayOff = Math.ceil(balance / monthly);
+
+      for (var i = 0; i < monthsToPayOff; i++) {
+        ret.push({
+          date: 'dno yet',
+          amountPaid: monthly,
+          newBalance: balance - (monthly * i + monthly)
+        });
+      }
+    }
+
+    return ret;
+  }.property('balance', 'interest', 'monthly'),
+
   actions: {
 
-    showTable: function () {
+    calculate: function () {
       this.set('canShowTable', true);
     }
   }
